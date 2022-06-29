334b30dd80d2cd53641272e62d53e61fe9ea8473
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2022-06-29 19:59:49 +0300

    Add files via upload

--- 1st Homework 25.06-26.06/5.volumeOfCylinder.js
+++ 1st Homework 25.06-26.06/5.volumeOfCylinder.js
@@ -3,3 +3,23 @@
 // Volume of a cylinder : V = πr2h
 // where r is the radius and h is the height of the cylinder.
 
+
+class Numbers{
+    constructor(diameter, height){
+        this.diameter = diameter;
+        this.height = height;
+    }
+    volume(){
+        let radius = this.diameter / 2;
+        let result = this.height * Math.PI * radius * radius;
+        console.log(`The Cylider volume is: ${result.toFixed(4)}`)
+    }
+}   
+let numbers = new Numbers(7, 4);
+numbers.volume();
+
+
+
+  
+
+
